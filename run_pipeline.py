91a8aa0ba63f098b4187a0089eb2ece6938f29f3
Prajwal <prajwalgbdr03@gmail.com>
2023-08-22 09:56:14 +0530

    project str added

---
+++ run_pipeline.py
@@ -0,0 +1,7 @@
+from pipelines.training_pipeline import train_pipeline
+
+
+
+if __name__ == '__main__':
+    #run the training pipeline
+    train_pipeline(data_path='D:/Projects/Customer-Satisfaction-Prediction-Using-ZenML/data/olist_customers_dataset.csv')
