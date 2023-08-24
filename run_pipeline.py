f68d5f55c64452b9fe543392cd8d995fa9fff06b
praj2408 <prajwalgbdr03@gmail.com>
2023-08-24 22:32:27 +0530

    run pipeline fixed

--- run_pipeline.py
+++ run_pipeline.py
@@ -4,4 +4,4 @@ from pipelines.training_pipeline import train_pipeline
 
 if __name__ == '__main__':
     #run the training pipeline
-    train_pipeline(data_path='D:/Projects/Customer-Satisfaction-Prediction-Using-ZenML/data/olist_customers_dataset.csv')
+    train_pipeline(data_path='./data/olist_customers_dataset.csv')
